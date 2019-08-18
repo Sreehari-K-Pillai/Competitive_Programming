17e9f37e4be521ee380f9777af21d11e52e99aed
Sreehari-K-Pillai <53552488+Sreehari-K-Pillai@users.noreply.github.com>
2019-08-18 20:19:59 +0530

    Create LowerBound.cpp

---
+++ LowerBound.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+
+
+int main() {
+    int N;
+    int query;
+    int queryLength;
+    {
+
+    std::vector<int>::iterator low;
+    std::vector<int>::iterator  foundValue;
+    std::cin >> N;
+
+    std::vector<int> v(N);
+     for(int i =0; i<N; i++){
+         std::cin >> v[i];
+     }
+
+    std::cin >> queryLength;
+
+    for(int j = 0; j < queryLength; j++){
+         std::cin >> query;
+         foundValue= find(v.begin(), v.end(), query);
+         if (foundValue != v.end())
+         {
+             std::cout << "Yes "<< (foundValue-v.begin()+ 1) << std::endl;
+         }
+          else{
+            low = std::lower_bound (v.begin(), v.end(),query);
+             std::cout << "No "<< find(v.begin(), v.end(), *low)-v.begin()+ 1 << std::endl;
+          }
+    }
+}
+    return 0;
+}
